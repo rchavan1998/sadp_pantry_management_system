acf82eb60e1cc4f1e8806f5ae85b5867fff46a99
arati <aratik@umich.edu>
2023-10-16 14:46:06 -0400

    adding springboot project

---
+++ spms_backend/src/main/java/com/pantrypal/pantrypal/PantrypalApplication.java
@@ -0,0 +1,13 @@
+package com.pantrypal.pantrypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantrypalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PantrypalApplication.class, args);
+	}
+
+}
